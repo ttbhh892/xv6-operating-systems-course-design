2470636e9138eed0c0d53680168241985759d301
Tao Zhian <1252971275@qq.com>
2026-08-13 16:53:30 +0800

    complete trace system call

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,6 +119,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->trace_mask = 0;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
@@ -288,6 +289,7 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  np->trace_mask = p->trace_mask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
