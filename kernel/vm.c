f03188acfa2158f28f752d5d785f5b1f9f532595
Tao Zhian <1252971275@qq.com>
2026-08-14 17:00:30 +0800

    handle copy on write in copyout

--- kernel/vm.c
+++ kernel/vm.c
@@ -413,21 +413,36 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
+  pte_t *pte;
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+
+    // 内核写入不会产生用户态缺页异常，因此在这里主动处理 COW。
+    pte = walk(pagetable, va0, 0);
+    if(pte == 0)
+      return -1;
+
+    if(*pte & PTE_COW){
+      if(cowalloc(pagetable, va0) < 0)
+        return -1;
+    }
+
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
+
     n = PGSIZE - (dstva - va0);
     if(n > len)
       n = len;
+
     memmove((void *)(pa0 + (dstva - va0)), src, n);
 
     len -= n;
     src += n;
     dstva = va0 + PGSIZE;
   }
+
   return 0;
 }
 
