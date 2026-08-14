07ae84007ac5abc390edfe9af4029572dabda37c
Tao Zhian <1252971275@qq.com>
2026-08-14 14:46:59 +0800

    complete sysinfo system call

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -80,3 +80,20 @@ kalloc(void)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
 }
+
+uint64
+freemem(void)
+{
+  struct run *r;
+  uint64 bytes = 0;
+
+  acquire(&kmem.lock);
+
+  for(r = kmem.freelist; r != 0; r = r->next)
+    bytes += PGSIZE;
+
+  release(&kmem.lock);
+
+  return bytes;
+}
+
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
 
 uint64
 sys_exit(void)
@@ -106,3 +107,23 @@ sys_trace(void)
   myproc()->trace_mask = mask;
   return 0;
 }
+
+uint64
+sys_sysinfo(void)
+{
+  uint64 addr;
+  struct sysinfo info;
+  struct proc *p = myproc();
+
+  if(argaddr(0, &addr) < 0)
+    return -1;
+
+  info.freemem = freemem();
+  info.nproc = nproc();
+
+  if(copyout(p->pagetable, addr, (char *)&info, sizeof(info)) < 0)
+    return -1;
+
+  return 0;
+}
+
